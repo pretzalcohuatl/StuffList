e22817497f86f04ef6fd23bebba77870499c4b44
William <12982372+pretzalcohuatl@users.noreply.github.com>
2020-05-26 12:51:50 -0400

    Add files via upload

---
+++ StuffList/Source/StuffList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace StuffList
+{
+    [StaticConstructorOnStartup]
+    public static class StuffList
+    {
+        static StuffList()
+        {
+
+        }
+
+    }
+}
